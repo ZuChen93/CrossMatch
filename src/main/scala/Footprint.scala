365330383bc1a2abd03bd324e35d34c0d50a342e
田祖宸 <tzc9338@gmail.com>
2017-03-27 17:15:06 +0800

    整理代码

--- src/main/scala/Footprint.scala
+++ src/main/scala/Footprint.scala
@@ -1,51 +1,116 @@
 import java.io.File
-import java.lang.Math.{cos, pow, sqrt, toRadians}
 
-import org.apache.spark.{HashPartitioner, Partitioner, SparkConf, SparkContext}
-
-import scala.math.LowPriorityOrderingImplicits
-
+import org.apache.spark._
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
 
 /**
   * Created by Chen on 2017/3/5 0005.
   */
 object Footprint {
+
+  val N_side: Int = 15
+
   def main(args: Array[String]): Unit = {
     val file2MASS = ".\\res\\2MASS_OUTPUT.txt"
     val fileSDSS = ".\\res\\SDSS_OUTPUT.txt"
-    //    val output = ".\\res\\output.txt"
+    // val output = ".\\res\\output.txt"
     val output = "D:\\footprint"
 
-    //    deleteFile(new File(output))
+    // deleteFile(new File(output))
 
-
-    val N_side = 15
-    val conf = new SparkConf().setAppName("CrossMatch").setMaster("local").setSparkHome(System.getenv("SPARK_HOME"))
+    val conf = new SparkConf().setAppName("CrossMatch").setMaster("spark://ip:7077").setSparkHome(System.getenv("SPARK_HOME"))
     val sc = new SparkContext(conf)
 
-    val data2MASS = sc.textFile(file2MASS).map(dataImporter)
-    val dataSDSS = sc.textFile(fileSDSS).map(dataImporter)
+    //    val data2MASS = sc.textFile(file2MASS).map(dataImporter)
+    //    val dataSDSS = sc.textFile(fileSDSS).map(dataImporter)
 
+    var firstRound = sc.broadcast(true)
 
-    val twoOrderData = data2MASS.map {
-      case (healpixId, coordinateTuple) => (twoOrder(healpixId, 2), coordinateTuple)
+    var keepData: Broadcast[Array[((Long, Byte), (Double, Double))]] = null
+
+    for (i <- 13 to 14) {
+      var twoOrderData: RDD[((Long, Byte), (Double, Double))] = null
+
+      if (firstRound.value) {
+        // 把读入文件的过程应该放进循环里比较好吧？
+        val data2MASS = sc.textFile(file2MASS).map(dataImporter)
+        /*
+        * HealPix分解成 上一级高位High+本级低位Low 形式
+        * ((281,1),(0.3601450701050618,1.5874289965420452))
+        * ((280,3),(0.18082391104499088,1.58617386963976))
+        * ((278,2),(0.47050725285909084,1.5789050249842687))
+        * ((281,1),(0.32655967199906594,1.579745990452854))
+        * ((292,3),(6.071633457316581,1.5865394497965875))
+        * */
+        twoOrderData = data2MASS.map {
+          case (healpixId, coordinateTuple) => (twoOrder(healpixId, i), coordinateTuple)
+        }
+        firstRound = sc.broadcast(false)
+        println("first")
+      }
+      else {
+        println("next")
+        twoOrderData = sc.makeRDD(keepData.value).map {
+          case ((highOrder, _), coordinateTuple) => (twoOrder(highOrder, i), coordinateTuple)
+        }
+      }
+
+      /*
+    * 去重，方便后面统计低位个数（00、01、10、11）
+    * todo:直接对healpix去重不得了？？？
+    * ((278,2),(0.47050725285909084,1.5789050249842687))
+    * ((281,1),(0.32655967199906594,1.579745990452854))
+    * ((292,3),(6.071633457316581,1.5865394497965875))
+    * */
+      val distinctTwoOrderData = twoOrderData.reduceByKey((v1, v2) => v2)
+      /*
+    * 同一HighOrder的LowOrder归类
+    * (74,List(1))
+    * (90,List(0, 2))
+    * (72,List(3))
+    * (70,List(1, 0, 2))
+    * (68,List(3))
+    * (73,List(1, 3, 2, 0))
+    * (75,List(0))
+    * (143,List(3))
+    * (69,List(0, 1, 2))
+    * (67,List(3))
+    * */
+      val highOrderSingleData = distinctTwoOrderData.map { case ((highOrder, lowOrder), coordinate) => highOrder -> lowOrder }
+        // 转成Byte方便循环时的模式匹配，不知道会不会降低性能
+        .combineByKey[List[Byte]](
+        (list: Byte) => List(list),
+        (list: List[Byte], lowOrder: Byte) => list ++ List(lowOrder),
+        (v1: List[Byte], v2: List[Byte]) => v1 ++ v2
+      )
+      // 另一种方法，用aggregateByKey实现
+      //val highOrderSingleData = distinctTwoOrderData.map { case ((highOrder, lowOrder), coordinate) => highOrder -> lowOrder }
+      //  .aggregateByKey(Nil: List[Long])((list: List[Long], lowOrder: Long) => list ++ List(lowOrder), (v1, v2) => v1 ++ v2)
+
+      // 筛选出满足条件的High分开，作为过滤器，分别对原数据集过滤
+      /**
+        * 此方法不妥，collect操作会将数据传回driver，数据量大的时候可能有性能损耗，
+        * 似乎只要不以RDD形式存在，数据就会传回到driver内存中。
+        *
+        * @note this method should only be used if the resulting array is expected to be small, as
+        *       all the data is loaded into the driver's memory.
+        *       但是不用collect，filter还是RDD形式存在，没有像Array一样的contains方法
+        **/
+      val pickupDataFilter = highOrderSingleData.filter(_._2.size < 4).map(_._1)
+      val keepDataFilter = highOrderSingleData.filter(_._2.size == 4).map(_._1)
+      // 直接调用过滤器变量会报错，只能用广播的方法再把过滤器变量广播到所有worker上
+      val bcPickupDataFilter = sc.broadcast(pickupDataFilter.collect)
+      val bcKeepDataFilter = sc.broadcast(keepDataFilter.collect)
+      // 不向上聚合的数据
+      val pickupData = twoOrderData.filter(v => bcPickupDataFilter.value.contains(v._1._1))
+      // TODO: 直接存到hdfs中？一堆文件合不合适？
+      pickupData.saveAsTextFile(output + "\\" + i)
+      // TODO: 或者collect传回driver？费不费内存？
+      // val tempData=pickupData.collect()
+      // 继续聚合的数据
+      keepData = sc.broadcast(twoOrderData.filter(v => bcKeepDataFilter.value.contains(v._1._1)).collect)
     }
-    val distinctTwoOrderData = twoOrderData.reduceByKey((v1, v2) => v2)
-    //    var highOrderSingleData = distinctTwoOrderData.map { case ((highOrder, lowOrder), coordinate) => highOrder -> lowOrder }
-    //      .combineByKey[List[Long]](
-    //      (list: Long) => List(list),
-    //      (list: List[Long], lowOrder: Long) => list ++ List(lowOrder),
-    //      (v1: List[Long], v2: List[Long]) => v1 ++ v2
-    //    )
-    // 另一种方法
-    val highOrderSingleData = distinctTwoOrderData.map { case ((highOrder, lowOrder), coordinate) => highOrder -> lowOrder }
-      .aggregateByKey(Nil: List[Long])((list: List[Long], lowOrder: Long) => list ++ List(lowOrder), (v1, v2) => v1 ++ v2)
-
-
-    val pickupDataFilter = highOrderSingleData.filter(_._2.size < 4).map(_._1)
-    val keepDataFilter = highOrderSingleData.filter(_._2.size == 4).map(_._1)
-    val mask = sc.broadcast(pickupDataFilter.collect)
-    twoOrderData.filter(v => mask.value.contains(v._1._1)).foreach(println)
 
     // ID -> 1, 仿照wordcount计数
     // dataSDSS.map(pixel => (highOrder(pixel._1, 14), 1)).reduceByKey(_ + _).filter(v => v._2 > 2).saveAsTextFile(output)
@@ -63,6 +128,8 @@ object Footprint {
     //      (v: (Long, Long, Double, Double)) => new List(v._1) -> new List(v._2, v._3, v._4),
     //      (value: C, tuple: (Long, Long, Double, Double)) =>
 
+    /*
+     *  构建简单数据测试
     var orders = List[Int]()
     val array = sc.parallelize((1 to 24).toList ++ List(2, 3, 4, 5, 6)).cache
 
@@ -77,6 +144,7 @@ object Footprint {
         else
           idList.map(id => ((id, 2), id))
       }.filter { case ((_, order), _) => order != 2 }
+    */
 
     // TODO: 循环？？
     // TODO: treeReduce怎么用???
@@ -123,14 +191,14 @@ object Footprint {
 
 
   // 给一个order序列，生成id对应的order集合
-  def highOrder(id: Long, order: Int, highestOrder: Int = 15): Long = id >> (highestOrder - order) * 2
+  def highOrder(id: Long, order: Int, highestOrder: Int = N_side): Long = id >> (highestOrder - order) * 2
 
-  //  def lowOrder(id: Long, order: Int, highestOrder: Int = 15): Long = (id >> (highestOrder - order) * 2) & 0x3
-  def lowOrder(id: Long, order: Int, highestOrder: Int = 15): Long = (id >> (highestOrder - order - 1) * 2) & 0x3
+  //  def lowOrder(id: Long, order: Int, highestOrder: Int = N_side): Long = (id >> (highestOrder - order) * 2) & 0x3
+  def lowOrder(id: Long, order: Int, highestOrder: Int = N_side): Long = (id >> (highestOrder - order - 1) * 2) & 0x3
 
-  def twoOrder(id: Long, order: Int, highestOrder: Int = 15): (Long, Long) = {
+  def twoOrder(id: Long, order: Int, highestOrder: Int = N_side): (Long, Byte) = {
     // val highOrder = id >> (highestOrder - order) * 2
-    (id >> (highestOrder - order) * 2, id >> (highestOrder - order - 1) * 2 & 0x3)
+    (id >> (highestOrder - order) * 2, (id >> (highestOrder - order - 1) * 2 & 0x3).toByte)
   }
 
   def toOrders(array: Array[Int], id: Long): Array[Long] = for (i <- array) yield highOrder(id, i)
